14439f8478fb61ee3637e0c6d67654e1d511785b
HoeksMJ <hoeksmj@gmail.com>
2023-05-11 16:23:11 -0500

    re-add comments for future reference

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-let container = document.getElementById("container");
+let container = document.getElementById("container");               //get HTML object with Id container
 
-let gridTotal = 100;
-let gridDimension = "80px";
-createGrid();
-drawGrid();
+let gridTotal = 100;                                                //set initial total amount of grid boxes to (10 x 10)
+let gridDimension = "80px";                                         //set initial gridDimension (width) of boxes to 1/10 of total width (800px)
+createGrid();                                                       //create an initial grid using gridTotal and grid Dimension (a 10x10 grid)
+drawGrid();                                                         //allows user to draw without having to press any grid dimension selector buttons
 
 
-let tenBtn = document.getElementById("tenBtn");
+let tenBtn = document.getElementById("tenBtn");                     //when (10 x 10) button is clicked, erase any created button elements, and make a new (10 x 10) grid
+eraseGrid();
 tenBtn.addEventListener("click", () => {
     eraseGrid();
     gridTotal = 100;
@@ -15,11 +16,11 @@ tenBtn.addEventListener("click", () => {
     drawGrid();
 });
 
-let sixteenBtn = document.getElementById("sixteenBtn");
+let sixteenBtn = document.getElementById("sixteenBtn");              //when (16 x 16) button is clicked, erase any created button elements and make a new (16 x 16) grid
 sixteenBtn.addEventListener("click", () => {
     eraseGrid();
-    gridTotal = 256;
-    gridDimension = "50px";
+    gridTotal = 256;                                                 //16 x 16
+    gridDimension = "50px";                                          //each box will be 1/16th of the height and width of container box (800px)
     createGrid();
     drawGrid();
 });
@@ -27,8 +28,8 @@ sixteenBtn.addEventListener("click", () => {
 let fourtyBtn = document.getElementById("fourtyBtn");
 fourtyBtn.addEventListener("click", () => {
     eraseGrid();
-    gridTotal = 1600;
-    gridDimension = "20px";
+    gridTotal = 1600;                                                //total number of boxes (buttons)
+    gridDimension = "20px";                                          //each box will be 1/40th of the height and width of container box (800px)
     createGrid();
     drawGrid();
 });
@@ -37,43 +38,43 @@ let hundredBtn = document.getElementById("hundredBtn");
 hundredBtn.addEventListener("click", () => {
     eraseGrid();
     gridTotal = 10000;
-    gridDimension = "8px";
+    gridDimension = "8px";                                           //each box will be 1/100th of the height and width of container box (800px)
     createGrid();
     drawGrid();
 });
 
-function createGrid (){
+function createGrid (){                                             //create i amount of boxes (buttons), determined by gridDimension, and assign each a unique id
     for (let i = 1; i <= gridTotal; i++) {
         let gridCreator = document.createElement("button");
         gridCreator.className = "grid";
         gridCreator.id = `grid${i}`;
         document.querySelector("#container").appendChild(gridCreator);
     };
-    for (let i = 1; i<= gridTotal; i++){
+    for (let i = 1; i<= gridTotal; i++){                            //set the dimension of each box, targeted via its newly created unique id, to be gridDimension (determined by the relative button pressed)
         let gridBox = document.getElementById(`grid${i}`);
         gridBox.style.maxWidth = gridDimension;
     };
 };
 
-function eraseGrid (){
+function eraseGrid (){                                              //target each element by its unique id, up to however many total boxes there are, determined by gridTotal, and remove
     for (let i = 1; i <= gridTotal; i++){
         let gridBox = document.getElementById(`grid${i}`);
         gridBox.remove();
     };
 };
 
-function drawGrid(){ 
+function drawGrid(){                                                //for each element targeted via its unique id, upt o however many total there are, determined by gridTotal, change the background color of a box when a mouse is pressed down and hovers onto the box
     for (let i = 1; i <= gridTotal; i++){
         let gridBox = document.getElementById(`grid${i}`);
-        let toggle = undefined;
+        let toggle = false;
         container.addEventListener("mousedown", () => {
-            toggle = 1;
+            toggle = true;
         });
         container.addEventListener("mouseup", () => {
-            toggle = 0;
+            toggle = false;
         });
         gridBox.addEventListener("mouseenter", () => {
-            if (toggle ===1){
+            if (toggle == true){
                 gridBox.style.backgroundColor = "rgba(241, 223, 197, 0.6)";
             };
         });
