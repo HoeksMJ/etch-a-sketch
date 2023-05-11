d62d7e862abd570c6037d9e0efac706d37fd9f57
HoeksMJ <hoeksmj@gmail.com>
2023-05-11 13:09:36 -0500

    add createGrid, drawGrid, and eventListeners to the four buttons

--- script.js
+++ script.js
@@ -1,18 +1,67 @@
 let container = document.getElementById("container");
 
 let gridTotal = 256;
+let gridDimension = "10px";
 
-for (let i = 1; i <= gridTotal; i++) {
-    let grid = document.createElement("button");
-    grid.className = "grid";
-    grid.id = `grid${i}`;
-    document.querySelector("#container").appendChild(grid);
+let tenBtn = document.getElementById("tenBtn");
+tenBtn.addEventListener("click", () => {
+    gridTotal = 100;
+    gridDimension = "80px";
+    createGrid();
+    drawGrid();
+});
+
+let sixteenBtn = document.getElementById("sixteenBtn");
+sixteenBtn.addEventListener("click", () => {
+    gridTotal = 256;
+    gridDimension = "50px";
+    createGrid();
+    drawGrid();
+});
+
+let fourtyBtn = document.getElementById("fourtyBtn");
+fourtyBtn.addEventListener("click", () => {
+    gridTotal = 1600;
+    gridDimension = "20px";
+    createGrid();
+    drawGrid();
+});
+
+let hundredBtn = document.getElementById("hundredBtn");
+hundredBtn.addEventListener("click", () => {
+    gridTotal = 10000;
+    gridDimension = "8px";
+    createGrid();
+    drawGrid();
+});
+
+function createGrid (){
+    for (let i = 1; i <= gridTotal; i++) {
+        let gridCreator = document.createElement("button");
+        gridCreator.className = "grid";
+        gridCreator.id = `grid${i}`;
+        document.querySelector("#container").appendChild(gridCreator);
+    };
+    for (let i = 1; i<= gridTotal; i++){
+        let gridBox = document.getElementById(`grid${i}`);
+        gridBox.style.maxWidth = gridDimension;
+    };
 };
 
-for (let i = 1; i<= gridTotal; i++) {
-    let btn = document.getElementById(`grid${i}`);
-    btn.addEventListener("mouseenter", () => {
-        btn.style.backgroundColor = "rgba(214, 214, 214, 0.5)";
-    });
+function drawGrid(){ 
+    for (let i = 1; i <= gridTotal; i++){
+        let gridBox = document.getElementById(`grid${i}`);
+        let toggle = undefined;
+        container.addEventListener("mousedown", () => {
+            toggle = 1;
+        });
+        container.addEventListener("mouseup", () => {
+            toggle = 0;
+        });
+        gridBox.addEventListener("mouseenter", () => {
+            if (toggle ===1){
+                gridBox.style.backgroundColor = "rgba(214, 214, 214, 0.5)";
+            };
+        });
+    };
 };
-
